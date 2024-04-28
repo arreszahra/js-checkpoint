68e2b60f2cadf44ca7e49286cc8b7de768320c80
Zahra Arres <arreszahra0@gmail.com>
2024-04-28 20:56:19 +0100

    second commit

--- check.js
+++ check.js
@@ -1,6 +1,111 @@
-stringstr="hello word";{
-    
-    reverse (srt.begin(),srt.end());
-cout<<"hello word"<<str;
-return 0;
+
+/*
+
+******Task1******
+function reverseString(str) {
+    if (str === "")
+      return "";
+    else
+      return reverseString(str.substr(1)) + str.charAt(0);
+  }
+  reverseString("hello");
+  */
+
+/*
+******task2******
+
+  function countCharacters(str){
+    if (i=0 ; str.length>i ; i++) {
+   return(length.str)
+        
+    }
+  }
+  countCharacters("hello world");
+
+  */
+
+  /*
+
+  ******task3******
+   function upperCase(){
+    let text="hello world";
+    if ((text.length[1])&& (text="")){
+     result=text.toUpperCase();
+    }
+    console.log(result);
+   }
+  */
+
+
+ /*
+ ****task5****
+ array=[5,3,7,2,9,0];
+   function minAndMax (array){
+    var x=Math.max(...array);
+    var y=Math.min(...array);
+    return (x,y);
+   }
+   */
+/*
+ ******task6******
+   function somme(){
+let arr=[3,8,7,10,16];
+let sum=0;
+for ( i=0; arr.length>i; i++ ){
+    sum+=arr[i]
 }
+   }
+   console.log(sum);
+   */
+  /*
+
+  ******task7******
+     const score=[12,16,8,11,6,4];
+     const result=filter(score);
+
+     function tabFilter(score){
+          if score>= 10;{
+            return Passed;
+            else 
+            return Failed;
+          }
+   }
+   */
+
+/*
+
+******task8******
+   function fact(){
+    return n*n;
+   }
+*/
+
+
+/*
+   function primeNumberCheck(n){
+if ((n%1===0) && (n%n===0)) {
+    return ("n est un nombre premier") ;
+} 
+else 
+{
+  return   ("n  pas premier");
+}
+}
+
+*/
+
+/*
+******fibonacciSeries******
+
+const number = parseInt(prompt('Enter the number of terms: '));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
+*/
